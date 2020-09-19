e49c632dddbea342d84204b6d1515eec682fc7fa
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-09-20 00:31:49 +0800

    Place a order fucntionality finished

--- app/src/main/java/app/noobstack/bringme/bringmelk/BuyActivity.java
+++ app/src/main/java/app/noobstack/bringme/bringmelk/BuyActivity.java
@@ -1,10 +1,14 @@
 package app.noobstack.bringme.bringmelk;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,13 +17,20 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
-public class BuyActivity extends AppCompatActivity{
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import app.noobstack.bringme.bringmelk.model.Order;
+
+public class BuyActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView buyImageView;
     private TextView buyTextTitle;
@@ -31,10 +42,102 @@ public class BuyActivity extends AppCompatActivity{
     private String currentUserId;
     private String currentUserEmail;
 
+    private EditText buyerName;
+    private EditText buyerMobile;
+    private EditText buyerAddress;
+    private int count;
+    private double total;
+    private double discount;
+    private double oneUnitPrice;
+    private Button counterTag;
+    private Button totalPriceTag;
+    private Button incrementBtn, decrementBtn, confirmBtn;
+
+    private DatabaseReference OrderDB;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.increment:
+                if(count < 20){
+                    count++;
+                    String initialCounterTag = count + " UNITS";
+                    counterTag.setText(initialCounterTag);
+                    total = (count * oneUnitPrice) - (count * oneUnitPrice * discount/100.0);
+
+                    String initialTotalTag = "TOTAL PRICE " + total;
+                    totalPriceTag.setText(initialTotalTag);
+                }else{
+                    Toast.makeText(this, "Maximum 20 units can be delivered at a time", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.decrement:
+                if(count > 1){
+                    count--;
+                    String initialCounterTag = count + " UNITS";
+                    counterTag.setText(initialCounterTag);
+                    total = (count * oneUnitPrice) - (count * oneUnitPrice * discount/100.0);
+
+                    String initialTotalTag = "TOTAL PRICE " + total;
+                    totalPriceTag.setText(initialTotalTag);
+                }else{
+                    Toast.makeText(this, "You should order at least 1 unit", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.confirmBtn:
+                String buyer_Name = buyerName.getText().toString().trim();
+                String buyer_Mobile = buyerMobile.getText().toString().trim();
+                String buyer_Address = buyerAddress.getText().toString().trim();
+                String requested_Time = LocalDateTime.now().toString();
+                String payment_status = "pending";
+                String user_Id = currentUserId;
+                String driver_Id = "not assigned";
+                String prepared_Time = "not prepared";
+                String delivered_time = "not delivered";
+                String Total_Price = Double.toString(total);
+                String order_Id = UUID.randomUUID().toString();
+
+                if(!TextUtils.isEmpty(buyer_Name) && !TextUtils.isEmpty(buyer_Mobile) && !TextUtils.isEmpty(buyer_Address)){
+                    if(buyer_Mobile.length() != 10 || !buyer_Mobile.startsWith("0")){
+                        Toast.makeText(this, "Enter a valid mobile number", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Order newOrder = new Order();
+                        newOrder.setBuyer_Name(buyer_Name);
+                        newOrder.setBuyer_Mobile(buyer_Mobile);
+                        newOrder.setBuyer_Address(buyer_Address);
+                        newOrder.setRequested_Time(requested_Time);
+                        newOrder.setPayment_status(payment_status);
+                        newOrder.setUser_Id(user_Id);
+                        newOrder.setDriver_Id(driver_Id);
+                        newOrder.setPrepared_Time(prepared_Time);
+                        newOrder.setDelivered_time(delivered_time);
+                        newOrder.setTotal_Price(Total_Price);
+                        newOrder.setOrder_Id(order_Id);
+
+                        OrderDB.child(order_Id).setValue(newOrder);
+
+                        Toast.makeText(this, "Order has been placed", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(this, "Fill out the fields", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+
+
+
+        }
+    }
+
     public BuyActivity() {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         currentUserId = currentUser.getUid();
         currentUserEmail = currentUser.getEmail();
+        count = 1;
+        total = 1.00;
     }
 
     @Override
@@ -42,13 +145,33 @@ public class BuyActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         buyImageView = findViewById(R.id.buy_food_image);
         buyTextTitle = findViewById(R.id.buy_food_title);
         buyTextDesc = findViewById(R.id.buy_food_desc);
         buyTextPrice = findViewById(R.id.buy_food_price);
         buyTextDiscount = findViewById(R.id.buy_food_discount);
 
+        buyerName = findViewById(R.id.buyerName);
+        buyerMobile = findViewById(R.id.buyerContactNo);
+        buyerAddress = findViewById(R.id.buyerAddress);
+        incrementBtn = findViewById(R.id.increment);
+        decrementBtn = findViewById(R.id.decrement);
+        counterTag = findViewById(R.id.TotalUnitTag);
+        totalPriceTag = findViewById(R.id.TotalPriceTag);
+        confirmBtn = findViewById(R.id.confirmBtn);
+
+        String initialCounterTag = count + "UNITS";
+        counterTag.setText(initialCounterTag);
+
+        incrementBtn.setOnClickListener(this);
+        decrementBtn.setOnClickListener(this);
+        confirmBtn.setOnClickListener(this);
+
         Intent intent =getIntent();
+
+        OrderDB = FirebaseDatabase.getInstance().getReference().child("orders");
 
         String buy_food_title = intent.getStringExtra("ITEM_TITLE").toString();
         String buy_food_desc = intent.getStringExtra("ITEM_DESC").toString();
@@ -61,6 +184,15 @@ public class BuyActivity extends AppCompatActivity{
         buyTextDesc.setText(buy_food_desc);
         buyTextPrice.setText(buy_food_price);
         buyTextDiscount.setText(buy_food_discount);
+
+        discount = Double.parseDouble(buy_food_discount);
+        oneUnitPrice = Double.parseDouble(buy_food_price);
+
+        total = oneUnitPrice - (oneUnitPrice * discount/100.0);
+
+        String initialTotalTag = "TOTAL PRICE " + total;
+        totalPriceTag.setText(initialTotalTag);
+
 
         Picasso.get().load(buy_food_image).networkPolicy(NetworkPolicy.OFFLINE).into(buyImageView, new Callback() {
             @Override
---
+++ app/src/main/java/app/noobstack/bringme/bringmelk/model/Order.java
@@ -0,0 +1,123 @@
+package app.noobstack.bringme.bringmelk.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Order {
+    String buyer_Name;
+    String buyer_Mobile;
+    String buyer_Address;
+    String requested_Time;
+    String payment_status;
+    String user_Id;
+    String driver_Id;
+    String prepared_Time;
+    String delivered_time;
+    String Total_Price;
+    String order_Id;
+
+    public Order() {
+    }
+
+    public Order(String buyer_Name, String buyer_Mobile, String buyer_Address, String requested_Time, String payment_status, String user_Id, String driver_Id, String prepared_Time, String delivered_time, String total_Price, String order_Id) {
+        this.buyer_Name = buyer_Name;
+        this.buyer_Mobile = buyer_Mobile;
+        this.buyer_Address = buyer_Address;
+        this.requested_Time = requested_Time;
+        this.payment_status = payment_status;
+        this.user_Id = user_Id;
+        this.driver_Id = driver_Id;
+        this.prepared_Time = prepared_Time;
+        this.delivered_time = delivered_time;
+        Total_Price = total_Price;
+        this.order_Id = order_Id;
+    }
+
+    public String getBuyer_Name() {
+        return buyer_Name;
+    }
+
+    public void setBuyer_Name(String buyer_Name) {
+        this.buyer_Name = buyer_Name;
+    }
+
+    public String getBuyer_Mobile() {
+        return buyer_Mobile;
+    }
+
+    public void setBuyer_Mobile(String buyer_Mobile) {
+        this.buyer_Mobile = buyer_Mobile;
+    }
+
+    public String getBuyer_Address() {
+        return buyer_Address;
+    }
+
+    public void setBuyer_Address(String buyer_Address) {
+        this.buyer_Address = buyer_Address;
+    }
+
+    public String getRequested_Time() {
+        return requested_Time;
+    }
+
+    public void setRequested_Time(String requested_Time) {
+        this.requested_Time = requested_Time;
+    }
+
+    public String getPayment_status() {
+        return payment_status;
+    }
+
+    public void setPayment_status(String payment_status) {
+        this.payment_status = payment_status;
+    }
+
+    public String getUser_Id() {
+        return user_Id;
+    }
+
+    public void setUser_Id(String user_Id) {
+        this.user_Id = user_Id;
+    }
+
+    public String getDriver_Id() {
+        return driver_Id;
+    }
+
+    public void setDriver_Id(String driver_Id) {
+        this.driver_Id = driver_Id;
+    }
+
+    public String getPrepared_Time() {
+        return prepared_Time;
+    }
+
+    public void setPrepared_Time(String prepared_Time) {
+        this.prepared_Time = prepared_Time;
+    }
+
+    public String getDelivered_time() {
+        return delivered_time;
+    }
+
+    public void setDelivered_time(String delivered_time) {
+        this.delivered_time = delivered_time;
+    }
+
+    public String getTotal_Price() {
+        return Total_Price;
+    }
+
+    public void setTotal_Price(String total_Price) {
+        Total_Price = total_Price;
+    }
+
+    public String getOrder_Id() {
+        return order_Id;
+    }
+
+    public void setOrder_Id(String order_Id) {
+        this.order_Id = order_Id;
+    }
+}
