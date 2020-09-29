d7aac1ee82084327816088537ca3dac264e29b40
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-09-29 16:24:52 +0800

    loading progress added

--- app/src/main/java/app/noobstack/bringme/bringmelk/LoadingActivity.java
+++ app/src/main/java/app/noobstack/bringme/bringmelk/LoadingActivity.java
@@ -5,15 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class LoadingActivity extends AppCompatActivity {
     private static  int SPLASH_TIME_OUT = 2000;
+    private ProgressBar progressBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        progressBar = findViewById(R.id.progressLoad);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i =0; i<10; i++){
+                    progressBar.incrementProgressBy(10);
+                    SystemClock.sleep(500);
+                }
+            }
+        });
+
+        thread.start();
 
         //hide the top title bar
         try {
