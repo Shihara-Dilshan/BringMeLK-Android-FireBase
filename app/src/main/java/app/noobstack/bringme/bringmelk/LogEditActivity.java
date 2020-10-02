5501546f3a4f5e2c71f41e275ebd8a6246f01607
umesha-D <umeshadsamanayaka@gmail.com>
2020-10-02 23:15:02 +0530

    log management final UI

---
+++ app/src/main/java/app/noobstack/bringme/bringmelk/LogEditActivity.java
@@ -0,0 +1,13 @@
+package app.noobstack.bringme.bringmelk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_edit);
+    }
+}
