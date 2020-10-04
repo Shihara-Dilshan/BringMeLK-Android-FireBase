0effec82e66ce0f33c199c084e69ac7d1836844e
Maryam-Yumna <maryamyumna@gmail.com>
2020-10-04 13:38:22 +0530

    food management test cases added

---
+++ app/src/test/java/app/noobstack/bringme/bringmelk/IT19055404/AddNewFoodTest.java
@@ -0,0 +1,63 @@
+package app.noobstack.bringme.bringmelk.IT19055404;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import app.noobstack.bringme.bringmelk.AddNewFood;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AddNewFoodTest {
+
+    boolean validPrice;
+    boolean validDiscount;
+    static AddNewFood newFood;
+
+
+
+    @Before
+    public  static void createObject(){
+        newFood = new AddNewFood();
+    }
+
+    @Test
+    public void isValidDiscount(){
+        validDiscount = newFood.isValidDiscount(20);
+        assertTrue(validDiscount);
+
+    }
+    @Test
+    public void isInvalidDiscount(){
+        validDiscount = newFood.isValidDiscount(-1);
+        assertFalse(validDiscount);
+    }
+    @Test
+    public void isInvalidDiscount2(){
+        validDiscount = newFood.isValidDiscount(101);
+        assertFalse(validDiscount);
+    }
+
+    @Test
+    public void isInvalidPrice(){
+        validPrice = newFood.isValidPrice(-2);
+        assertFalse(validPrice);
+    }
+    @Test
+    public void isValidPrice(){
+        validPrice = newFood.isValidPrice(500);
+        assertTrue(validPrice);
+    }
+
+    @After
+    public static void deleteObject(){
+        newFood = null;
+    }
+
+
+
+}
+
